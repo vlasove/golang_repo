591de040827a9970197f2b361c57f8e460d997c5
Evgen <evgeny_vlasov@yahoo.com>
2019-02-11 02:03:06 +0300

    Add .CSV read func

--- user/tfs/first_hw.go
+++ user/tfs/first_hw.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
 
+type MegaStruct struct {
+	Company   string
+	PriceOpen float64
+	Value     int
+	TimeStamp time.Time
+}
+
+func Filer(path string) []MegaStruct {
+	var arr []MegaStruct
+	csvFile, err := os.Open(path)
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+	r := csv.NewReader(csvFile)
+	for {
+		records, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		price, _ := strconv.ParseFloat(records[1], 64)
+		value, _ := strconv.Atoi(records[2])
+		layout := "2006-01-02 15:04:05.000000"
+		//str := "2014-11-12T11:45:26.371Z"
+		t, _ := time.Parse(layout, records[3])
+		arr = append(arr, MegaStruct{
+			Company:   records[0],
+			PriceOpen: price,
+			Value:     value,
+			TimeStamp: t,
+		})
+	}
+	return arr
+
+}
 func main() {
-	fmt.Println("Repo created")
+
+	newObj := Filer("trades.csv")
+	for i := 0; i < len(newObj); i++ {
+		fmt.Println(newObj[i].Company, newObj[i].PriceOpen, newObj[i].Value, newObj[i].TimeStamp)
+		fmt.Println(newObj[i].TimeStamp.Minute())
+		//TODO добавить функцию прохода окном с заданной длиной
+		//TODO функция сортировки полученных данных
+		//TODO функция записи в файл полученных данных
+	}
+
 }
