da1a6e9b8dc82338b4b13bb8987ad871e2b7d278
Evgeny Vlasov <evgeny_vlasov@yahoo.com>
2019-02-24 13:32:04 +0300

    Pre-worked commit

--- user/simple_project/web/main.go
+++ user/simple_project/web/main.go
@@ -8,7 +8,7 @@ import (
 func index_handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Whoa, Golang is awesome!</h1>")
 	fmt.Fprintf(w, "<p>Go fast and simple</p>")
-	//fmt.Fprintf(w, "<p>You can %v even add %v </p>", "pidor", "<strong>variables</strong>")
+	fmt.Fprintf(w, "<p>You can %v even add %v </p>", "pidor", "<strong>variables</strong>")
 }
 
 func about_handler(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +18,6 @@ func about_handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", index_handler)
 	//http.HandleFunc("/about/", about_handler)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":3000", nil)
 
 }
